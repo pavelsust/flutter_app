8ab66baac782d7d79fec0f143543c124a7753d7f
paveltech <pavelcep.sust11@gmail.com>
2020-03-26 14:58:20 +0600

    working with toolbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-
-    MaterialApp(
-      title: 'First App',
-      home: Material(
+  runApp(MaterialApp(
+    title:'My App',
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text('My Toolbar')
+      ),
+      body: Material(
         color: Colors.lightBlueAccent,
         child: Center(
-          child: (
-          Text('hello world',
-          textDirection: TextDirection.ltr)
-          ),
+          child: Text('Hello world' ,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white ,
+          fontSize: 20)
+          )
         ),
       ),
-    )
-  );
+    ),
+  ));
 }
