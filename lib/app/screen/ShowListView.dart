79796a4cb577f52de38ba478cc5b9eb06cf0c15b
paveltech <pavelcep.sust11@gmail.com>
2020-04-06 23:27:45 +0600

    list added

--- lib/app/screen/ShowListView.dart
+++ lib/app/screen/ShowListView.dart
@@ -4,20 +4,50 @@ import 'package:flutter/material.dart';
 class ShowListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     var myItems = makeList();
     var listContainer = Container(
       child: ListView.builder(itemBuilder: (context, index) {
         return ListTile(
           leading: Icon(Icons.landscape),
           title: Text(myItems[index]),
-          onTap: (){
+          onTap: () {
             debugPrint('click position $index and item is ${myItems[index]}');
+            showSnakebar(context, index);
           },
         );
       }),
     );
     return listContainer;
+  }
+
+  void showSnakebar(BuildContext context, var item) {
+    var snackBar = SnackBar(
+      content: Text('item number ${item}'),
+      action: SnackBarAction(
+        label: 'Undo',
+        onPressed: () {
+          debugPrint('Performing dummy Undo operation');
+          showUndoSnackBar(context, item);
+        },
+      ),
+    );
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  void showUndoSnackBar(BuildContext context, var item) {
+    var snackBar = SnackBar(
+      content: Text(
+        '$item undo done',
+        textDirection: TextDirection.rtl,
+        style: TextStyle(
+          color: Colors.lightBlue,
+          fontSize: 10,
+        ),
+      ),
+      backgroundColor: Colors.deepPurple,
+    );
+
+    Scaffold.of(context).showSnackBar(snackBar);
   }
 }
 
