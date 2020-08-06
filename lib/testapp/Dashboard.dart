ab1d9055936e147670c46f46fed17744811176e5
paveltech <pavelcep.sust11@gmail.com>
2020-08-06 13:14:43 +0600

    dashboard list design

--- lib/testapp/Dashboard.dart
+++ lib/testapp/Dashboard.dart
@@ -37,118 +37,182 @@ class MainContent extends StatelessWidget {
                       'Explore',
                       textAlign: TextAlign.left,
                       style: TextStyle(
-                        fontSize: 20,
+                        fontSize: 23,
                       ),
                     ),
                   ],
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Color(0xFFFD7384)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.drive_eta, color: Colors.white),
+                              Text(
+                                'Motor',
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 100,
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Color(0xFFFDD093),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 8.0),
+                                        child: Icon(Icons.local_offer,
+                                            color: Colors.white),
+                                      ),
+                                      Text(
+                                        'Classified',
+                                        style: TextStyle(
+                                            fontSize: 15, color: Colors.white),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Color(0xFF2BD093),
+                                  ),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Icon(Icons.local_offer,
+                                          color: Colors.white),
+                                      Text(
+                                        'Classified',
+                                        style: TextStyle(
+                                            fontSize: 15, color: Colors.white),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 100,
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Color(0xFFFC7B4D),
+                                  ),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 8.0),
+                                        child: Icon(Icons.account_balance,
+                                            color: Colors.white),
+                                      ),
+                                      Text(
+                                        'Balance',
+                                        style: TextStyle(
+                                            fontSize: 15, color: Colors.white),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Color(0xFF53CEDB),
+                                  ),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 8.0),
+                                        child: Icon(Icons.access_time,
+                                            color: Colors.white),
+                                      ),
+                                      Text(
+                                        'Time',
+                                        style: TextStyle(
+                                            fontSize: 15, color: Colors.white),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Row(
                   children: <Widget>[
                     Expanded(
-                      child: Container(
-                        height: 100,
-                        width: 100,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            color: Color(0xFFFD7384)),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(Icons.drive_eta, color: Colors.white),
-                            Text(
-                              'Motor',
-                              style:
-                                  TextStyle(fontSize: 15, color: Colors.white),
-                            )
-                          ],
+                      child: Text(
+                        'Popular Trending',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 3,
                     ),
                     Expanded(
-                      child: Container(
-                        height: 100,
-                        child: Column(
-                          children: <Widget>[
-                            Expanded(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: Color(0xFFFDD093),
-                                ),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    Icon(Icons.local_offer, color: Colors.white),
-                                    Text(
-                                      'Classified',
-                                      style:
-                                      TextStyle(fontSize: 15, color: Colors.white),
-                                    )
-                                  ],
-                                ),
-                              ),
-
-                            ),
-                            SizedBox(
-                              height: 3,
-                            ),
-                            Expanded(
-                              child: Container(
-                                width:  double.infinity,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: Color(0xFF2BD093),
-                                ),
-                                child: Column(
-                                  children: <Widget>[
-                                    Icon(Icons.local_offer, color: Colors.white),
-                                    Text(
-                                      'Classified',
-                                      style:
-                                      TextStyle(fontSize: 15, color: Colors.white),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            )
-                          ],
+                      child: Text(
+                        'View All',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                            fontSize: 18,
+                          color: Color(0xFF2BD093),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 3,
-                    ),
-                    Expanded(
-                      child: Container(
-                        height: 100,
-                        child: Column(
-                          children: <Widget>[
-                            Expanded(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: Color(0xFFFC7B4D),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 3,
-                            ),
-                            Expanded(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: Color(0xFF53CEDB),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
+                    )
                   ],
                 )
               ],
